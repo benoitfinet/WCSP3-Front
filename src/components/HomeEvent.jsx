1eab24ac9b7fb6199926c3a5abf4355a726ea7ae
LHippolyteCBM <lefebure.hippolyte@gmail.com>
2021-01-06 15:55:30 +0100

    home-event V1

---
+++ src/components/HomeEvent.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { MDBRow, MDBCol, MDBCard, MDBCardBody, MDBMask, MDBView, MDBBtn } from 'mdbreact';
+
+const BlogPage = () => {
+  return (
+    <MDBCard className="my-5 px-5 pb-5">
+      <MDBCardBody>
+        <MDBRow>
+          <MDBCol lg="5">
+            <MDBView className="rounded z-depth-2 mb-lg-0 mb-4" hover waves>
+              <img
+                className="img-fluid"
+                src="https://mdbootstrap.com/img/Photos/Others/img%20(27).jpg"
+                alt=""
+              />
+              <a href="#!">
+                <MDBMask overlay="white-slight" />
+              </a>
+            </MDBView>
+          </MDBCol>
+          <MDBCol lg="7">
+            <a href="#!" className="green-text">
+              <h6 className="font-weight-bold mb-3">
+                Évenement
+              </h6>
+            </a>
+            <h3 className="font-weight-bold mb-3 p-0">
+              <strong>Nocturne</strong>
+            </h3>
+            <p>
+                Evénements Alti’Max propose des mois de Mai à Septembre une soirée nocturne tous les premiers samedi du mois et à partir de 6 ans.
+                Réservation obligatoire, places limitées.
+                Fête des mères et fête de pères. 50% sur le prix de l’entrée pour le parent pratiquant sous condition de venir en famille de trois personnes minimum. Privatisation du parc ou pour toutes demandes particulières, contactez-nous : altimax78@gmail.com
+            </p>
+            <MDBBtn color="success" size="md" className="waves-light ">
+              En savoir plus
+            </MDBBtn>
+          </MDBCol>
+        </MDBRow>
+        <hr className="my-5" />
+        <MDBRow>
+          <MDBCol lg="7">
+            <a href="#!" className="pink-text">
+              <h6 className="font-weight-bold mb-3">
+                Offre
+              </h6>
+            </a>
+            <h3 className="font-weight-bold mb-3 p-0">
+              <strong>Privatisez votre terrasse</strong>
+            </h3>
+            <p>
+                Privatisez votre terrasse Vous pouvez privatiser votre terrasse aérienne à la journée.
+                Equipé d’une table, de bancs et d’un coffre fermé par cadenas afin d’y déposer vos affaires.
+                Vous pourrez ainsi profiter de votre pause déjeuner, gouter ou anniversaire en hauteur avec la vue sur le parc et à l’écart.
+            </p>
+            <MDBBtn color="pink" size="md" className="mb-lg-0 mb-4 waves-light">
+              En savoir plus
+            </MDBBtn>
+          </MDBCol>
+          <MDBCol lg="5">
+            <MDBView className="rounded z-depth-2 mb-lg-0 mb-4" hover waves>
+              <img
+                className="img-fluid"
+                src="https://mdbootstrap.com/img/Photos/Others/img%20(34).jpg"
+                alt=""
+              />
+              <a href="#!">
+                <MDBMask overlay="white-slight" />
+              </a>
+            </MDBView>
+          </MDBCol>
+        </MDBRow>
+
+      </MDBCardBody>
+    </MDBCard>
+  );
+};
+
+export default BlogPage;
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 import './index.css';
 import App from './App';
+import BlogPage from './components/HomeEvent';
+import Footer from './components/Footer';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <BlogPage />
+    <Footer />
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
