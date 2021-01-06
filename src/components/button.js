8c990d3a9cd73b687d67ef63fdc4e382de8b432e
benoitfinet <benoitfinetpro@gmail.com>
2021-01-06 11:33:13 +0100

    add carousel for home page

---
+++ src/components/button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './button.css';
+
+function button () {
+  return (
+        <div>
+            <button className="button">Découvrez toutes nos activités</button>
+        </div>
+  );
+}
+
+export default button;
