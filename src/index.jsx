18bdba22da8e9d49e2227b104b0be510d6120d9d
LHippolyteCBM <lefebure.hippolyte@gmail.com>
2021-01-07 10:38:07 +0100

    home-event V3 modif d'import dans App.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -5,13 +5,11 @@ import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import './index.css';
 import App from './App';
-import BlogPage from './components/HomeEvent';
 import Footer from './components/Footer';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <BlogPage />
     <Footer />
   </React.StrictMode>,
   document.getElementById('root')
